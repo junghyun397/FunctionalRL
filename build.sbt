9a3748a3ddfa40ecf0bb5042f073ea20136e22b8
junghyun397 <junghyun397@gmail.com>
2020-02-04 11:09:55 +0900

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "FunctionalRL"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
---
+++ src/main/scala/functional/rl/FunctionalRL.scala
@@ -0,0 +1,8 @@
+package functional.rl
+
+object FunctionalRL {
+
+  def main(args: Array[String]): Unit = println("Hello, Functional RL!")
+
+}
+
